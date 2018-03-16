913832e50b6799a87e9827a727419c15de6fa8ed
Lukasz Dycjan <lukasz.dycjan@ocado.com>
2018-03-16 15:12:13 +0100

    Char parser

--- src/main/scala/ParserCombinatorsWithState.scala
+++ src/main/scala/ParserCombinatorsWithState.scala
@@ -1,13 +1,13 @@
-class ParserCombinatorsWithState {
+object ParserCombinatorsWithState {
 
   /** Aim is to be able to parse the following:
-    *  var x = 1;
-    *  var y = x * 3;
-    *  x + y;
-    *  (x + 1) * 3 + 1;
-    *  var z = 8;
+    * var x = 1;
+    * var y = x * 3;
+    * x + y;
+    * (x + 1) * 3 + 1;
+    * var z = 8;
     *
-    *  with the following result: (Map(x -> 1, y -> 3, z -> 8), List(4, 7))
+    * with the following result: (Map(x -> 1, y -> 3, z -> 8), List(4, 7))
     */
 
   /** Step 1: Parsing of simple arithmetic language:
@@ -16,4 +16,17 @@ class ParserCombinatorsWithState {
     * 3*3+4*4*(1+2);
     */
 
+  trait Parser[A] {
+    def run: String => Option[(A, String)]
+  }
+
+  def char(c: Char) = new Parser[Char] {
+    def run = input => input.headOption.filter(_ == c).map((_, input.tail))
+  }
+
+  def main(args: Array[String]) = {
+    println(
+      char('a').run("aaaaa")
+    )
+  }
 }
